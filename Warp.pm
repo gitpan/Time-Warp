9ad7f80e374ea66a49d00367f2724aadba0fa592
Joshua Nathaniel Pritikin <jpritikin@pobox.com>
1999-06-24 19:07:16 +0000

    Import of JPRIT/Time-Warp-0.04 from CPAN.
    gitpan-cpan-distribution: Time-Warp
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         JPRIT/Time-Warp-0.04.tar.gz
    gitpan-cpan-author:       JPRIT
    gitpan-cpan-maturity:     released

--- Warp.pm
+++ Warp.pm
@@ -5,7 +5,7 @@ require Exporter;
 require DynaLoader;
 @ISA =         qw(DynaLoader Exporter);
 @EXPORT_OK   = qw(reset to scale time);
-$VERSION     = '0.03';
+$VERSION     = '0.04';
 
 __PACKAGE__->bootstrap($VERSION);
 
